53fdf2999cf58a2d80269f918a6916c5e41a1e56
Jessica Almand <jlalmand27@gmail.com>
2022-11-28 22:32:51 -0500

    Added and finished user model, except for matching validation for email

--- models/user.js
+++ models/user.js
@@ -1,1 +1,45 @@
-const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            // validate email address
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+        {
+            toJSON: {
+              virtuals: true,
+            },
+            id: false,
+          },
+    
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
